ed0b39daa27d337ff5f076d6e8c478f4bd49de60
Maros Zatko <mzatko@redhat.com>
2012-08-27 00:58:52 +0200

    added support for mutation, added tracing, type synonyms, rewrote runPopulation, minor debugging stuff, crossing runs parallel

--- Genet.hs
+++ Genet.hs
@@ -1,24 +1,58 @@
 module Genet where
 
 import Data.List
+import Debug.Trace
+import Control.Parallel.Strategies
 
-objectiveFun reference test = (/2) $ (**2) $ sum $ map (uncurry (-)) (zip reference test)
+objectiveFun reference test = (**2) $ sum $ map (\(x,y) -> {-trace ("x:"++show (x!!0) ++ " y:"++show (y!!0)) $ -} abs $ (x!!0) - (y!!0)) (zip reference test)
 
-runPopulation fn input population = map (uncurry fn) $ zip population input
+
+type Individuum = [[[Double]]]
+type Input      = [Double]
+type Output     = [Double]
+runPopulation :: (Individuum -> Input -> Output) 
+              -> [Input] -> [Individuum] -> [[Output]]
+runPopulation fn inputs population =
+    map (\individual -> map (fn individual) inputs) population
+--map (uncurry fn) $ zip population input
 
 testPopulation reference results = map (objectiveFun reference) results
 
 -- errors are results from testPopulation
-selectPop population errors = take 10 $ sortBy (\(x,_) (y,_)-> compare x y) $ zip errors population
+selectPop population errors = take 10 $ sortBy (\(x,_) (y,_) -> compare x y) $ zip errors population
 
+crossPopulation :: (a -> a -> a) -> [a] -> [a]
 crossPopulation matingF population = [matingF a b | a <- population, b <- population]
 
-runGen matingF fn maxerror population input reference = let
-  pop  = runPopulation fn input population
+--mutatePopulation :: MutatingF -> [Individuum] -> ([Individuum], [Double])
+mutateIndividuum individuum rands = level1 individuum rands
+  where
+    level3 items rnd = (zipWith (\x y -> x+(y*2-1)/10000.0) items $ take (length items) rnd
+                       ,drop (length items) rnd)
+    level2 items rnd = foldl (\(i,rs) y -> let (x',y') = level3 y rs in (x':i,y')) ([], rnd) items
+    level1 items rnd = foldl (\(i,rs) y -> let (x',y') = level2 y rs in (x':i,y')) ([], rnd) items
+
+mutatePopulation population rands = foldl (\(i,rs) y -> let (x',y') = mutateIndividuum y rs in (x':i,y')) ([], rands) population
+
+
+runGen :: (Individuum -> Individuum -> Individuum)
+       -> (Individuum -> Input -> Output)
+       -> Double -> [Individuum] -> [Input] -> [Output] -> [Double] -> Int -> (Double, Individuum)
+runGen matingF fn maxerror population inputs reference rands iii = let
+  pop  = runPopulation fn inputs population
   test = testPopulation reference pop
   sele = selectPop population test
-  npop = crossPopulation matingF $ map (uncurry const) sele
+  npop = crossPopulation matingF $ (parMap rpar (uncurry (flip const)) sele)
+  err  = fst (head sele)
+  (mpop, nrands) = mutatePopulation npop rands
   in
-    if fst (head sele) < maxerror
-      then head sele
-      else runGen matingF fn maxerror npop input reference
+    if iii == 10 then error ("SELE:" ++ show sele ++ "\nTEST:" ++ show test)
+    else
+      if err < maxerror
+        then trace (show err) $ head sele
+        else
+          trace "pllm:" $
+          trace (show err) $
+          trace ("sele:" ++ show sele) $
+          trace ("test:" ++ show test ++ "\n") $
+          runGen matingF fn maxerror mpop inputs reference nrands (iii+1)
